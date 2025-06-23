e9cad94a22e87e7f4846028a30b791805f404add
Mehari Fitihamlak <meharifitih@gmail.com>
2025-06-23 14:42:17 +0300

    fix subscription uniqueness

---
+++ database/migrations/2025_06_23_113646_modify_unique_on_subscriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropUnique('subscriptions_title_unique');
+            $table->unique(['title', 'interval']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropUnique(['title', 'interval']);
+            $table->unique('title', 'subscriptions_title_unique');
+        });
+    }
+};
